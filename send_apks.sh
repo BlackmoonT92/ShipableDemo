5c429b14fd64a8a746c48e65743f0d80ce52d274
TamPhan <tam@mysquar.com>
2015-04-18 22:34:28 +0700

    [debug-1] hello

--- send_apks.sh
+++ send_apks.sh
@@ -7,9 +7,9 @@ cp "app/build/outputs/apk/app-debug.apk" "app/build/outputs/apk/$COMMIT_COUNT-ap
 export TITLE_TAG=`[[ "$1" = "" ]] && echo "ShippaleTest" || echo "$1"`;
 echo "$TITLE_TAG"
 
-curl -s --user 'api:key-608cab8fe064fb580524ddeea2ce5596' \
-    https://api.mailgun.net/v3/sandboxa039bc7b423843f8a05dceae76962e29.mailgun.org/messages \
-    -F from='Mailgun Sandbox <postmaster@sandboxa039bc7b423843f8a05dceae76962e29.mailgun.org>' \
+curl -s --user 'api:key-b3e9f2ad4703dfd929d29c30760e2dde' \
+    https://api.mailgun.net/v2/sandbox94d84e7939014d6ca27590f77d582e06.mailgun.org/messages \
+    -F from='The Rocker <postmaster@sandbox94d84e7939014d6ca27590f77d582e06.mailgun.org>' \
     -F to='Tam Phan <tampv92@gmail.com>'\
     -F subject="[$TITLE_TAG] MyChat $COMMIT_COUNT" \
     -F text="`./release_notes.sh`" \
